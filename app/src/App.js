f20db41eb0b42409478a11e6938d92042632f945
Jakub Żołnacz <jakubzol@student.agh.edu.pl>
2018-11-30 23:10:54 +0100

    Front Login 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,42 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LoginBox from './login.js';
 
-class App extends Component {
-    state = {
-        isLoading: true,
-        users: []
-    };
-
-    async componentDidMount() {
-        const response = await fetch('/api/users');
-        const body = await response.json();
-        this.setState({ users: body, isLoading: false });
-    }
-
-    render() {
-        const {users, isLoading} = this.state;
-
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
-
-        return (
-            <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className="App-intro">
-            <h2>JUG DUPA</h2>
-        {users.map(user =>
-        <div key={user.id}>
-            {user.name}
+class App extends Component{
+    render(){
+        return(
+            <div>
+            <LoginBox />
             </div>
-        )}
-    </div>
-        </header>
-        </div>
-    );
+    )
     }
 }
+
+
+
 
 export default App;
