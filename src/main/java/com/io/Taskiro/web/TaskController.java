f47f2cb38c2a5c3f0bf034f426406e9204b147ad
bartoszgaj <bartosz.gaj4@gmail.com>
2018-12-01 17:01:13 +0100

    Login api for frontend

--- src/main/java/com/io/Taskiro/web/TaskController.java
+++ src/main/java/com/io/Taskiro/web/TaskController.java
@@ -1,8 +1,8 @@
 package com.io.Taskiro.web;
 
 
-import com.io.Taskiro.model.Task;
-import com.io.Taskiro.model.TaskRepository;
+import com.google.maps.model.LatLng;
+import com.io.Taskiro.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -21,14 +23,16 @@ class TaskController {
 
     private final Logger log = LoggerFactory.getLogger(TaskController.class);
     private TaskRepository taskRepository;
+    private UserRepository userRepository;
 
-    public TaskController(TaskRepository taskRepository) {
+    public TaskController(TaskRepository taskRepository, UserRepository userRepository) {
         this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
     }
 
-    @GetMapping("/tasks")
-    Collection<Task> tasks() {
-        return taskRepository.findAll();
+    @GetMapping("/users/{userId}/tasks")
+    Collection<Task> getAllTasksByUserId(@PathVariable (value= "userId") Long userId) {
+        return taskRepository.findByUserId(userId);
     }
 
     @GetMapping("/task/{id}")
@@ -38,13 +42,36 @@ class TaskController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping("/task")
-    ResponseEntity<Task> createTask(@Valid @RequestBody Task task) throws URISyntaxException {
+    @PostMapping("/users/{userId}/tasks")
+    ResponseEntity<Task> createTask(@PathVariable (value="userId") Long userId,
+                                    @RequestParam String title,
+                                    @RequestParam String description,
+                                    @RequestParam TaskType type,
+                                    @RequestParam Double lat,
+                                    @RequestParam Double lng,
+                                    @RequestParam Integer price,
+                                    @RequestParam String deadline,
+                                    @RequestParam String addTime
+
+                                    ) throws URISyntaxException {
+
+        Task task = new Task(title,description,type,new LatLng(lat,lng),price,
+                LocalDateTime.parse(deadline),LocalDateTime.parse(addTime));
         log.info("Request to create task: {}", task);
-        Task result = taskRepository.save(task);
-        return ResponseEntity.created(new URI("/api/task/" + result.getId()))
-                .body(result);
+        Optional<User> user = userRepository.findById(userId);
+        if(user.get() != null) {
+            task.setUser(user.get());
+            Task result = taskRepository.save(task);
+            return ResponseEntity.created(new URI("/api/" + userId + "/tasks/" + result.getId()))
+                    .body(result);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
+
+    @GetMapping("/tasks")
+    Collection<Task> tasks() {return taskRepository.findAll();}
 
     @PutMapping("/task")
     ResponseEntity<Task> updateTask(@Valid @RequestBody Task task) {
