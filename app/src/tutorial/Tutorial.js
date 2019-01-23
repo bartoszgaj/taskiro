252c0d39e33ba70b4f990260cb0be4a4bdb3b128
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-01-23 17:29:07 +0100

    fixed versions and tutorial added

--- app/src/tutorial/Tutorial.js
+++ app/src/tutorial/Tutorial.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import  { Document } from 'react-pdf';
+import user_doc from '../resources/taskiro_user_doc.pdf'
+
 
 
 class Tutorial extends Component{
@@ -7,7 +9,7 @@ class Tutorial extends Component{
         return(
             <div id="tutorial-box">
                 HAHA
-                <Document file={"taskiro_user_doc.pdf"} />
+                <Document file={"user_doc"} />
             </div>
         );
     }
